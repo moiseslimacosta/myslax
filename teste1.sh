60b206d2553f2037b37e56c68d387f7fdee35fe7
moiseslimacosta <moiseslimacosta@gmail.com>
2024-03-13 04:48:38 -0300

    Add files via upload

---
+++ teste1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "teste 1"
+sleep 3s
+
+echo "teste 2"
+sleep 3s
+
+echo "teste 3"
+sleep 3s
+
---
+++ teste2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "teste 5"
+sleep 3s
+
+echo "teste 6"
+sleep 3s
+
+echo "teste 7"
+sleep 3s
