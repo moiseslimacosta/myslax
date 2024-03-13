be01cb11eab2365e844662f7d114fbe3f93dd5d1
moiseslimacosta <moiseslimacosta@gmail.com>
2024-03-13 15:44:31 -0300

    Cleaning

--- jmusicbot.sh
+++ jmusicbot.sh
@@ -6,8 +6,7 @@ mv JMusicBot.service /etc/systemd/system/
 wget https://raw.githubusercontent.com/moiseslimacosta/myslax/main/config.txt
 mv config.txt /home/moises/jmusicbot
 mkdir /home/moises/jmusicbot/Playlist
-wget https://raw.githubusercontent.com/moiseslimacosta/myslax/main/jmusicbot/input.sh
-./input.sh | bash
+
 wget https://github.com/jagrosh/MusicBot/releases/download/0.4.0/JMusicBot-0.4.0.jar
 mv JMusicBot-0.4.0.jar /home/moises/jmusicbot/jmusicbot.jar
 systemctl start JMusicBot
