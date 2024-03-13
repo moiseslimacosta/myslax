1eb0375f9acd13b6608ee594bded663bd37e7496
moiseslimacosta <moiseslimacosta@gmail.com>
2024-03-13 16:07:26 -0300

    Update jmusicbot.sh

--- jmusicbot.sh
+++ jmusicbot.sh
@@ -6,9 +6,26 @@ mv JMusicBot.service /etc/systemd/system/
 wget https://raw.githubusercontent.com/moiseslimacosta/myslax/main/config.txt
 mv config.txt /home/moises/jmusicbot
 mkdir /home/moises/jmusicbot/Playlist
-
 wget https://github.com/jagrosh/MusicBot/releases/download/0.4.0/JMusicBot-0.4.0.jar
 mv JMusicBot-0.4.0.jar /home/moises/jmusicbot/jmusicbot.jar
+
+echo "Open config file insert token and owner"
+echo "save and type exit"
+sleep 5
+echo "nano /home/moises/jmusicbot/config.txt"
+su - root
+echo "config is done?"
+echo "press enter to continue."
+read -t  50000
+read_exit_status=$?
+if [ $read_exit_status = 0 ]; then
+clear
+echo "continue"
+else
+clear
+echo "Back to CLI"
+fi
+
 systemctl start JMusicBot
 systemctl enable JMusicBot
 
