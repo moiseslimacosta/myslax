0e26488e9e71486721666ac4774d49ee59165836
moiseslimacosta <moiseslimacosta@gmail.com>
2024-03-13 18:55:07 -0300

    Update servers.sh

--- servers.sh
+++ servers.sh
@@ -7,18 +7,20 @@ curl -sSL https://install.pi-hole.net | bash
 clear
 
 
+echo "installing Linux-Dash"
+sleep 3s
+git clone --depth 1 https://github.com/afaqurk/linux-dash.git
+mv linux-dash /var/www/html/
+clear
+
+
 echo "installing HomeBridge"
 sleep 3s
 curl -sSfL https://repo.homebridge.io/KEY.gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/homebridge.gpg  > /dev/null
-
 echo "deb [signed-by=/usr/share/keyrings/homebridge.gpg] https://repo.homebridge.io stable main" | sudo tee /etc/apt/sources.list.d/homebridge.list > /dev/null
-
 sudo apt-get update -y
-
 sudo apt-get install homebridge -y
-
 sudo wget https://raw.githubusercontent.com/moiseslimacosta/myslax/main/homebridge/config.json
-
 sudo mv config.json /var/lib/homebridge/
 
 clear
